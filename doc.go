e461d66fdf53e65dc2a6b3a99e935074a2c063b1
raphael <simon.raphael@gmail.com>
2014-12-04 23:48:13 -0800

    Update package doc to reflect README

--- doc.go
+++ doc.go
@@ -7,19 +7,15 @@
 // Action definitions leverage media types to describe the responses sent back to clients. Media types are used to
 // create documentation that describe the response contents.
 //
-// Automatic validation means that by the time your controller action is called, all the parameters (parsed from the
-// request path) and payload attributes (parsed from the request body) are of the expected type which means that your
-// code can focus on business logic.
+// Automatic validation means that by the time the controller action is called, all the parameters (parsed from the
+// request path) and payload attributes (parsed from the request body) are of the expected type.
 //
 // Automatic documentation makes it possible to design or change an exiting api and review the changes before writing
 // any action implementation code.
 //
 // goa also provides the following benefits:
-//
 // - built-in support for bulk actions using multi-part mime
-//
 // - integration with negroni to leverage existing middlewares
-//
 // - built-in support for form encoded, multipart form encoded and JSON request bodies
 //
 // Controllers in goa can be of any type. They must implement functions with the same name as the action name defined
@@ -31,6 +27,10 @@
 // attributes as well as the possible responses. goa will validate both the incoming requests and the controller
 // response against the definition. See the Resource type for more information.
 //
-// Once a controller and its resource definition are implemented they can be mounted onto the goa application. Mounting
+// Once a resource and the correspondin controller are implemented they can be mounted onto the goa application. Mounting
 // a controller defines its path. See the Mount function.
+//
+// The code is intented to be well documented so that it's possible to more easily browse through it and understand how
+// the library fits together. The "examples" directory contains a couple of simple examples to help get started. More
+// feature-ful examples are being worked on.
 package goa
